a77dc3e58c3906a7a1a9e61f9aeb887cbc8da8b4
RodrigoOler <roodrigoprogrammer@gmail.com>
2014-10-25 01:17:20 -0200

    Modificação do arquivo 03objects.rb

--- 03objects/03objects.rb
+++ 03objects/03objects.rb
@@ -5,10 +5,10 @@
     pois o Ruby vai cuidar das conversões automaticamente.
 =end
 
-puts 1.class
-puts self.class
-puts [].class
-puts ({}.class)
-puts "a".class
-puts 1.1.class
-puts 999999999999999.class
+puts 1.class #Fixnum
+puts self.class #Object
+puts [].class #Array
+puts ({}.class) #Hash
+puts "a".class #String
+puts 1.1.class #Float
+puts 999999999999999.class #Bignum
