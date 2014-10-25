c06154e9cc731227043998992b87d48acc90e131
RodrigoOler <roodrigoprogrammer@gmail.com>
2014-10-25 01:20:30 -0200

    Modificação do arquivo 02soma.rb

--- 02soma/02soma.rb
+++ 02soma/02soma.rb
@@ -1,3 +1,8 @@
+=begin
+    Os parênteses são opcionais em quase todas as situações.
+    Por exemplo, poderíamos reescrever o exemplo anterior da seguinte forma (01soma.rb)
+=end
+
 #Registrando um endereço para que o carteiro interno do Ruby possa entregar a mensagem a um objeto.
 def soma(a, b)
     a + b
