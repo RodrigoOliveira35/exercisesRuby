4fa88935f49cda280714b67591e8db7207258d5d
RodrigoOler <roodrigoprogrammer@gmail.com>
2014-10-25 01:19:01 -0200

    Modificação do arquivo 02soma.rb

--- 02soma/02soma.rb
+++ 02soma/02soma.rb
@@ -1,6 +1,6 @@
-#Definindo o método soma usando parênteses
+#Registrando um endereço para que o carteiro interno do Ruby possa entregar a mensagem a um objeto.
 def soma(a, b)
     a + b
 end
-#Imprimindo a soma entre a variavel A e B
+#Imprimindo a soma entre a variavel A e B.
 puts soma(1, 2);
